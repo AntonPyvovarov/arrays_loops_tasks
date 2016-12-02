5369ab30b31c9a85e96b978baf12629a8459b216
AntonPyvovarov <Antopyvo87@gmail.com>
2016-12-02 12:26:39 +0200

    array and cycle

---
+++ 4.php
@@ -0,0 +1,8 @@
+<?php
+$arr=['green'=>'Зелений','red'=>'Червоний','blue'=>'Синій'];
+foreach ($arr as $key=>$value){
+    echo $key.'<br>';
+}
+foreach ($arr as $key=>$value) {
+    echo $value . '<br>';
+}
