541853a18d3fecda14165eb6b1269ca7aeecf05b
AntonPyvovarov <Antopyvo87@gmail.com>
2017-01-07 19:58:46 +0200

    Array and cycles

--- 27.php
+++ 27.php
@@ -1,1 +1,53 @@
 <?php
+$row = rand(5, 9);
+$cols = rand(5, 20);
+$colors = ['red', 'yellow', 'blue', 'gray', 'maroon', 'brown', 'green'];
+$count = count($colors);
+//$randIndex = rand(0, $count - 1);
+
+/*style="background-color: <?php echo $color ?>"*/
+//create table
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<table border="1">
+    <tbody >
+    <?php
+    for ($i = 1; $i <= $row; $i++) {$randIndex = rand(0, $count - 1);
+        foreach ($colors as $key=>$value){
+            if ($randIndex===$key){
+                $color=$value;
+            }
+        }?>
+        <tr style="background-color: <?php echo $color ?>">
+            <td  >
+                <?php echo rand(0, 400); ?>
+            </td><?php
+            for ($j = 1; $j <= $cols; $j++) {
+                $randIndex = rand(0, $count - 1);
+                foreach ($colors as $key=>$value){
+                    if ($randIndex===$key){
+                        $color=$value;
+                    }
+                }?>
+                <td style="background-color: <?php echo $color ?>" >
+                <?php echo rand(0, 400); ?>
+                </td><?php }; ?></tr>
+        <?php } ?>
+
+    </tbody>
+</table>
+
+</body>
+</html>
