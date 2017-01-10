6ba78fa3064d00f84c6b1db26546d1dd51375a91
AntonPyvovarov <Antopyvo87@gmail.com>
2017-01-10 20:14:20 +0200

    finished task

--- 25.php
+++ 25.php
@@ -9,4 +9,17 @@ print_r($arr);
 echo '</pre>';
 echo 'максимальне значення = '.max($arr).'<br>';
 echo 'мінімальне значення = '.min($arr).'<br>';
-
+foreach ($arr as $key=>$value){
+    if(max($arr)==$value){
+        $max=$key;
+        echo $max."<br>";
+    }
+    if (min($arr)==$value){
+        $min=$key;
+        echo $min."<br>";
+    }
+}
+list($arr[$max],$arr[$min])=array($arr[$min],$arr[$max]);
+echo'<pre>';
+print_r($arr);
+echo '</pre>';
